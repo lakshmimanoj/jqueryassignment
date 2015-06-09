286edc2a8174bd88f831244838a697f986acb995
lakshmi_manoj <lakshmimanoj093@gmail.com>
2015-06-09 15:01:32 +0530

    completed assignment

---
+++ js/script.js
@@ -0,0 +1,83 @@
+$(document).ready(function(){
+var repoURL = "https://api.github.com/users/mralexgray/repos";
+var followersURL = "https://api.github.com/users/mralexgray/followers";
+var followingURL = "https://api.github.com/users/mralexgray/following";    
+    
+var $repoTable = $('#repoTable');
+var $followers = $('#followersSelection');
+var $following = $('#followingSelection');
+var $loader = $('#loader');
+var $followerDetails = $('#followersDetails');
+var $followingDetails = $('#followingDetails');
+    
+$loader.hide();
+fetchData(repoURL, $loader, "Repo Table", $repoTable);
+fetchData(followersURL, $loader, "Followers List", $followers);
+fetchData(followingURL, $loader, "Following List", $following);    
+    
+$followers.change(function() {
+    var $activitiesURL = "https://api.github.com/users/"+$( "#followersSelection option:selected" ).text()+"/received_events";
+    fetchData($activitiesURL, $loader, "Activities List", $followerDetails);
+});
+
+$following.change(function() {
+    var $activitiesURL = "https://api.github.com/users/"+$( "#followingSelection option:selected" ).text()+"/received_events";
+    fetchData($activitiesURL, $loader, "Activities List", $followingDetails); 
+});    
+$('button').click(function () {
+$("#list").remove();
+});
+});
+function fetchData (URL, loaderElement, fetch, dataElement) {
+$.ajax({
+type: 'GET',
+dataType: 'json',
+url: URL,
+cache: false,
+beforeSend: function(){
+loaderElement.show();
+$('dataElement').hide;
+},
+complete: function(){
+loaderElement.hide();
+$('input').show();
+dataElement.show();
+},
+success: function(json){
+if (fetch == "Repo Table"){
+populateRepoTable(json, dataElement);
+}
+else if (fetch == "Followers List"){
+populateComboBox(json, dataElement);
+}
+else if (fetch == "Following List"){
+populateComboBox(json, dataElement);
+}
+else {
+populateRecentActivities(json, dataElement);
+}
+}
+});
+}
+function populateRepoTable(json, repoTable){
+for(var i=0; i<json.length; i++){
+repoTable.append("<tr><th>"+json[i].name+"</th><th>"+json[i].full_name+"</th><th>"+json[i].size+"</th><th>"+json[i].created_at+"</th><th>"+json[i].language+"</th><th>"+json[i].has_issues+"</th><th>"+json[i].has_downloads+"</th><th>"+json[i].watchers+"</th></tr>");
+}
+}
+function populateComboBox(json, comboBox){
+for(var i=0; i<json.length; i++){
+comboBox.append("<option name="+i+"value="+json[i].login+">"+json[i].login+"</option>");
+}
+}
+function populateRecentActivities(json, activities){
+    
+    var latestDate=json[0].created_at, latestEvent=0;
+    for(var i=1; i<json.length; i++){
+        if(json[i].created_at > latestDate)
+            latestEvent=i;
+    }
+  
+    //$("#list").remove();
+    activities.append("<ul id='list'><li>Latest Event ID: "+json[latestEvent].id+"</li><li>Latest Event Type: "+json[latestEvent].type+"</li><li>Created Time: "+json[latestEvent].created_at+"</li></ul>");
+   
+}
